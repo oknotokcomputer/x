8ac9b186b90ddde03571ed0f6d3aff4a81067f07
Hung-Te Lin <hungte@chromium.org>
2017-01-10 18:24:51 +0800

    init: Change encrypted_stateful to skip encryption on factory mode.
    The factory software has to unmount stateful partition and rootfs during
    wiping process of finalization. The encstateful has created extra
    complexity and hard to solve service dependency for stopping (to prevent
    stateful/encstateful being busy). Also made it more difficult to debug
    devices when kernel is corrupted.
    
    Since "factory" mode for mount-encrypted was executed with fixed
    key, it should be fine to just skip encryption so the factory wiping
    process and fault analysis can be done more easily.
    
    Cherry-picked and modified from CL:426560
    
    BUG=chromium:679676
    TEST=./build_image test; install factory toolkit; run
         "gooftool wipe_in_place" and finished wiping process.
    
    Change-Id: Ic5e6bf5b106b4011e1f22b185a544aac2c48336f
    Reviewed-on: https://chromium-review.googlesource.com/494526
    Tested-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Commit-Queue: Hung-Te Lin <hungte@chromium.org>
    Trybot-Ready: Hung-Te Lin <hungte@chromium.org>

--- init/encrypted_stateful/startup_utils.sh
+++ init/encrypted_stateful/startup_utils.sh
@@ -12,6 +12,19 @@
 # partition, having /home/chronos already doesn't matter. It will be created by
 # mount-encrypted if it is missing. These mounts inherit nodev,noexec,nosuid
 # from the encrypted filesystem /mnt/stateful_partition/encrypted.
+
+# This should be same as unencrypted/startup_utils.sh
+mount_var_and_home_chronos_unencrypted() {
+  mkdir -p /mnt/stateful_partition/var || return 1
+  mount -n --bind /mnt/stateful_partition/var /var || return 1
+  mount -n --bind /mnt/stateful_partition/home/chronos /home/chronos
+}
+
 mount_var_and_home_chronos() {
-  mount-encrypted "$@" >/tmp/mount-encrypted.log 2>&1
+  if [ "$*" = "factory" ]; then
+    # chromium:679676 Factory tests may need to run in unencrypted environment.
+    mount_var_and_home_chronos_unencrypted
+  else
+    mount-encrypted "$@" >/tmp/mount-encrypted.log 2>&1
+  fi
 }

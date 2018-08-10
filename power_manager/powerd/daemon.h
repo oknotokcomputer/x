c55e59018d75c4da207c0ddcfa531189824575af
Josafat Garcia <josafat@chromium.org>
2018-08-10 05:14:43 +0000

    Revert "power: Implement CellularController"
    This reverts commit eaa64b646261e4159c39a66073bc4624c6bd8335.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > power: Implement CellularController
    > 
    > This class handles antenna power changes for the internal cellular modem
    > based on input from one or more proximity sensors, in the same vein
    > as WifiController
    > 
    > A TODO remains to actually implement and call the actual program that causes
    > the modem to alter its transmission power.
    > 
    > BUG=b:74586744
    > TEST=added unittest + observed logging on Nautilus
    > Signed-off-by: Enrico Granata <egranata@chromium.org>
    > 
    > Change-Id: I9455cb435c5cb6775d22c115878162c2395dc878
    > Reviewed-on: https://chromium-review.googlesource.com/1163828
    > Reviewed-by: Enrico Granata <egranata@chromium.org>
    > Tested-by: Enrico Granata <egranata@chromium.org>
    > (cherry picked from commit 2a08733a54f6aa6c554c09a15b4ffcdb2752c12f)
    > Reviewed-on: https://chromium-review.googlesource.com/1170113
    > Commit-Queue: Enrico Granata <egranata@chromium.org>
    
    Bug: b:74586744
    Change-Id: I508ee26cada6ce6dc7dea2bcfdd3ec0c80a40f92
    Reviewed-on: https://chromium-review.googlesource.com/1170118
    Reviewed-by: Josafat Garcia <josafat@chromium.org>
    Commit-Queue: Josafat Garcia <josafat@chromium.org>
    Tested-by: Josafat Garcia <josafat@chromium.org>
    Trybot-Ready: Josafat Garcia <josafat@chromium.org>

--- power_manager/powerd/daemon.h
+++ power_manager/powerd/daemon.h
@@ -20,7 +20,6 @@
 #include <dbus/exported_object.h>
 
 #include "power_manager/common/prefs_observer.h"
-#include "power_manager/powerd/policy/cellular_controller.h"
 #include "power_manager/powerd/policy/input_event_handler.h"
 #include "power_manager/powerd/policy/sar_handler.h"
 #include "power_manager/powerd/policy/suspender.h"
@@ -48,7 +47,6 @@ class MetricsCollector;
 
 namespace policy {
 class BacklightController;
-class CellularController;
 class InputDeviceController;
 class SarHandler;
 class StateController;
@@ -80,7 +78,6 @@ class Daemon;
 class Daemon : public policy::InputEventHandler::Delegate,
                public policy::Suspender::Delegate,
                public policy::WifiController::Delegate,
-               public policy::CellularController::Delegate,
                public system::AudioObserver,
                public system::DBusWrapperInterface::Observer,
                public system::PowerSupplyObserver {
@@ -137,9 +134,6 @@ class Daemon : public policy::InputEventHandler::Delegate,
 
   // Overridden from policy::WifiController::Delegate:
   void SetWifiTransmitPower(RadioTransmitPower power) override;
-
-  // Overridden from policy::CellularController::Delegate:
-  void SetCellularTransmitPower(RadioTransmitPower power) override;
 
   // Overridden from system::AudioObserver:
   void OnAudioStateChange(bool active) override;
@@ -278,7 +272,6 @@ class Daemon : public policy::InputEventHandler::Delegate,
   std::unique_ptr<system::DarkResumeInterface> dark_resume_;
   std::unique_ptr<policy::Suspender> suspender_;
   std::unique_ptr<policy::WifiController> wifi_controller_;
-  std::unique_ptr<policy::CellularController> cellular_controller_;
 
   std::unique_ptr<metrics::MetricsCollector> metrics_collector_;
 

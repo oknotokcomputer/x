edacef1736d96e629210bcafca810e185b7a13b4
Yi Chou <yich@google.com>
2020-10-08 23:25:06 +0800

    cryptohome: add has_reset_lock_permissions in RPC
    chromium side could use this to check the reset lock permission.
    
    http://crrev.com/c/2461977 depend on this CL.
    
    BUG=b:155063317
    TEST=build ok.
    TEST=FEATURES=test emerge-soraka cryptohome
    TEST=FEATURES=test emerge-chell cryptohome
    TEST=cryptohome --action=tpm_more_status
    
    Change-Id: Ia1166c61d0f69693d4e26e446014d81258510be5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2460975
    Tested-by: joe Chou <yich@google.com>
    Reviewed-by: Leo Lai <cylai@google.com>
    Commit-Queue: joe Chou <yich@google.com>
    (cherry picked from commit 69d769b0959ce5b4f850790129974d17c0a4945f)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2505280
    Reviewed-by: joe Chou <yich@google.com>

--- cryptohome/service.cc
+++ cryptohome/service.cc
@@ -3477,6 +3477,9 @@ void Service::DoGetTpmStatus(const brillo::SecureBlob& request,
     // already been destroyed.
     extension->set_initialized(extension->owned());
   }
+
+  extension->set_has_reset_lock_permissions(tpm_->HasResetLockPermissions());
+
   int counter;
   int threshold;
   bool lockout;

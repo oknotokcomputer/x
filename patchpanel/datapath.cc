a5a765ff36ea958d8fe81ae736dd3da814adbb0c
Hugo Benichi <hugobenichi@google.com>
2021-01-28 17:47:33 +0900

    patchpanel: ipv6: bypass POSTROUTING filters for ndp packets
    On 4.4 kernel and certain circumstance (ipv6 network), it is
    observed that routing connmark iptables rule can wrongly cause
    neighbor discovery packets to be dropped.
    
    Patch Change-Id I6ab04c8d2241f67d82b6b7eb939360f75cc71b84 added some
    iptables POSTROUTING filter rules involving connmark and fwmark
    arguments, allowing the above bug to happen and causing the loss of IPv6
    connectivity.
    
    This patch addresses this issue by adding explicit bypass in iptables
    POSTROUTING for IPv6 ndp packets.
    
    BUG=b:178674231
    TEST=Unit tests. Tested ping6 and tcp IPv6 connectivity on atlas on dual
    stack eth and wlan networks.
    
    Change-Id: I1ce419ff769888736233d0a6e52033266f114eb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2655086
    Tested-by: Hugo Benichi <hugobenichi@google.com>
    Commit-Queue: Hugo Benichi <hugobenichi@google.com>
    Reviewed-by: Taoyu Li <taoyl@chromium.org>
    Reviewed-by: Garrick Evans <garrick@chromium.org>
    (cherry picked from commit 52a649904937246cebff4965ec097e7f2e728145)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2662226
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>

--- patchpanel/datapath.cc
+++ patchpanel/datapath.cc
@@ -138,17 +138,6 @@ void Datapath::Start() {
     LOG(ERROR) << "Failed to set up NAT for TAP devices."
                << " Guest connectivity may be broken.";
 
-  // b/176260499: on 4.4 kernel, the following connmark rules are observed to
-  // wrongly cause neighbor discovery icmpv6 packets to be dropped. Add these
-  // rules to bypass connmark rule for those packets.
-  for (const auto& type : kNeighborDiscoveryTypes) {
-    if (!ModifyIptables(IpFamily::IPv6, "mangle",
-                        {"-A", "OUTPUT", "-p", "icmpv6", "--icmpv6-type", type,
-                         "-j", "ACCEPT", "-w"}))
-      LOG(ERROR) << "Failed to set up connmark bypass rule for " << type
-                 << " packets";
-  }
-
   // Applies the routing tag saved in conntrack for any established connection
   // for sockets created in the host network namespace.
   if (!ModifyConnmarkRestore(IpFamily::Dual, "OUTPUT", "-A", "" /*iif*/,
@@ -225,6 +214,22 @@ void Datapath::Start() {
                        kCheckRoutingMarkChain, "-w"}))
     LOG(ERROR) << "Failed to add POSTROUTING jump rule to "
                << kCheckRoutingMarkChain;
+
+  // b/176260499: on 4.4 kernel, the following connmark rules are observed to
+  // incorrectly cause neighbor discovery icmpv6 packets to be dropped. Add
+  // these rules to bypass connmark rule for those packets.
+  for (const auto& type : kNeighborDiscoveryTypes) {
+    if (!ModifyIptables(IpFamily::IPv6, "mangle",
+                        {"-I", "OUTPUT", "-p", "icmpv6", "--icmpv6-type", type,
+                         "-j", "ACCEPT", "-w"}))
+      LOG(ERROR) << "Failed to set up connmark bypass rule for " << type
+                 << " packets in OUTPUT";
+    if (!ModifyIptables(IpFamily::IPv6, "mangle",
+                        {"-I", kCheckRoutingMarkChain, "-p", "icmpv6",
+                         "--icmpv6-type", type, "-j", "RETURN", "-w"}))
+      LOG(ERROR) << "Failed to set up connmark bypass rule for " << type
+                 << " packets in " << kCheckRoutingMarkChain;
+  }
 }
 
 void Datapath::Stop() {

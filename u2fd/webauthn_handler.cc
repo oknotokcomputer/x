d5a2239497f3715ca9ad909579f393e83e49fc17
Yicheng Li <yichengli@chromium.org>
2021-02-03 12:18:37 -0800

    [M90] u2fd: Add UMA metrics to WebAuthn
    BUG=b:179195205
    TEST=unittests
    
    Change-Id: I6a1440ca0f0c69445a970211a1c93133130fab42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2679477
    Tested-by: Yicheng Li <yichengli@chromium.org>
    Commit-Queue: Yicheng Li <yichengli@chromium.org>
    Reviewed-by: Andrey Pronin <apronin@chromium.org>
    (cherry picked from commit e7a2625c4b1df5bed9662ca405696c23437e5eab)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2744363
    Reviewed-by: Yicheng Li <yichengli@chromium.org>

--- u2fd/webauthn_handler.cc
+++ u2fd/webauthn_handler.cc
@@ -73,6 +73,9 @@ constexpr char kCryptohomePinLabel[] = "pin";
 // Relative DBus object path for fingerprint manager in biod.
 const char kCrosFpBiometricsManagerRelativePath[] = "/CrosFpBiometricsManager";
 
+constexpr char kPerformingUserVerificationMetric[] =
+    "WebAuthentication.ChromeOS.UserVerificationRequired";
+
 std::vector<uint8_t> Uint16ToByteVector(uint16_t value) {
   return std::vector<uint8_t>({static_cast<uint8_t>((value >> 8) & 0xff),
                                static_cast<uint8_t>(value & 0xff)});
@@ -145,12 +148,14 @@ void WebAuthnHandler::Initialize(
     UserState* user_state,
     U2fMode u2f_mode,
     std::function<void()> request_presence,
-    std::unique_ptr<AllowlistingUtil> allowlisting_util) {
+    std::unique_ptr<AllowlistingUtil> allowlisting_util,
+    MetricsLibraryInterface* metrics) {
   if (Initialized()) {
     LOG(INFO) << "WebAuthn handler already initialized, doing nothing.";
     return;
   }
 
+  metrics_ = metrics;
   tpm_proxy_ = tpm_proxy;
   user_state_ = user_state;
   user_state_->SetSessionStartedCallback(
@@ -201,6 +206,7 @@ void WebAuthnHandler::OnSessionStarted(const std::string& account_id) {
     LOG(ERROR) << "Did not load all records for user " << *sanitized_user;
     return;
   }
+  webauthn_storage_->SendRecordCountToUMA(metrics_);
 }
 
 void WebAuthnHandler::OnSessionStopped() {
@@ -302,6 +308,7 @@ void WebAuthnHandler::MakeCredential(
 
   if (session.request.verification_type() ==
       VerificationType::VERIFICATION_USER_VERIFICATION) {
+    metrics_->SendBoolToUMA(kPerformingUserVerificationMetric, true);
     dbus::MethodCall call(
         chromeos::kUserAuthenticationServiceInterface,
         chromeos::kUserAuthenticationServiceShowAuthDialogMethod);
@@ -318,6 +325,7 @@ void WebAuthnHandler::MakeCredential(
     return;
   }
 
+  metrics_->SendBoolToUMA(kPerformingUserVerificationMetric, false);
   DoMakeCredential(std::move(session), PresenceRequirement::kPowerButton);
 }
 
@@ -879,6 +887,7 @@ void WebAuthnHandler::GetAssertion(
   if (session.request.verification_type() ==
           VerificationType::VERIFICATION_USER_VERIFICATION &&
       !is_legacy_credential) {
+    metrics_->SendBoolToUMA(kPerformingUserVerificationMetric, true);
     dbus::MethodCall call(
         chromeos::kUserAuthenticationServiceInterface,
         chromeos::kUserAuthenticationServiceShowAuthDialogMethod);
@@ -895,6 +904,7 @@ void WebAuthnHandler::GetAssertion(
     return;
   }
 
+  metrics_->SendBoolToUMA(kPerformingUserVerificationMetric, false);
   DoGetAssertion(std::move(session), PresenceRequirement::kPowerButton);
 }
 

48afc65f95958ed44357d1b3cdc5dc350f39729f
Taoyu Li <taoyl@google.com>
2021-03-11 17:21:30 +0900

    shill: vpn: check device state before ResetConnection()
    OnDefaultPhysicalServiceChanged event could be triggered before
    OnDriverConnected, when a VirtualDevice is not created yet. This
    can lead to shill crash and this patch fixes this bug.
    
    BUG=b:180713715
    TEST=unit
    
    Change-Id: I0c57caf0d01d706839a86e41c3635517214c06f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2750860
    Reviewed-by: Jie Jiang <jiejiang@chromium.org>
    Tested-by: Taoyu Li <taoyl@chromium.org>
    Commit-Queue: Taoyu Li <taoyl@chromium.org>
    (cherry picked from commit 2dae58fca79a82cc88af61a40b43f5a58406ff4b)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2790196
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>

--- shill/vpn/vpn_service.cc
+++ shill/vpn/vpn_service.cc
@@ -115,6 +115,10 @@ void VPNService::OnDriverFailure(ConnectFailure failure,
 void VPNService::OnDriverReconnecting(base::TimeDelta timeout) {
   StartDriverConnectTimeout(timeout);
   SetState(Service::kStateAssociating);
+  // If physical network changes before driver connection finished, this could
+  // be called before device_ was initialized.
+  if (!device_)
+    return;
   device_->ResetConnection();
 }
 

579ec757e287efb4ed0bb5a08e21663be0146bcb
Steven Bennetts <stevenjb@chromium.org>
2021-05-10 18:18:50 -0700

    shill: Cellular: Limit auto connect after failure prevention
    Logic was added to prevent Cellular auto-connect after failure
    to prevent repeated connect attempts while the Modem might be
    in a bad or unknown state.
    
    This can prevent legitimate connect attempts after a drop.
    The change limits the prevention logic to retry once the Modem
    has a chance to recover.
    
    Note: existing back-off logic will further limit autoconnect
    attempts after the delay.
    
    BUG=b:187646339
    TEST=unit tests
    
    Change-Id: I35fc2761a22caa211be2a8520b7a018c1e8800a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2885206
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Tested-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Pavan Holla <pholla@google.com>
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    (cherry picked from commit 22447a471f8ee5b2b94cd5db35d95711e8d0df3c)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2891694
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>

--- shill/cellular/cellular_service.cc
+++ shill/cellular/cellular_service.cc
@@ -11,8 +11,9 @@
 #include <base/notreached.h>
 #include <base/stl_util.h>
 #include <base/strings/string_number_conversions.h>
-#include <base/strings/stringprintf.h>
 #include <base/strings/string_util.h>
+#include <base/strings/stringprintf.h>
+#include <base/time/time.h>
 #include <chromeos/dbus/service_constants.h>
 
 #include "shill/adaptor_interfaces.h"
@@ -61,6 +62,9 @@ const char kStorageLastGoodAPN[] = "Cellular.LastGoodAPN";
 
 const char kApnVersionProperty[] = "version";
 const int kCurrentApnCacheVersion = 2;
+
+constexpr base::TimeDelta kAutoConnectFailedTime =
+    base::TimeDelta::FromSeconds(20);
 
 bool GetNonEmptyField(const Stringmap& stringmap,
                       const string& fieldname,
@@ -496,9 +500,12 @@ bool CellularService::IsAutoConnectable(const char** reason) const {
     return false;
   }
   if (failure() == kFailureConnect) {
-    // For Cellular, do not repeatedly auto connect after a failure.
-    *reason = kAutoConnConnectFailed;
-    return false;
+    base::Optional<base::TimeDelta> failed_time = GetTimeSinceFailed();
+    if (failed_time && *failed_time < kAutoConnectFailedTime) {
+      // For Cellular, do not immediately auto connect after a failure.
+      *reason = kAutoConnConnectFailed;
+      return false;
+    }
   }
   if (out_of_credits_) {
     *reason = kAutoConnOutOfCredits;

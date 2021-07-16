666e7d3c4b5c8d8c9b8d04423e0aafdf98c83af4
Steven Bennetts <stevenjb@chromium.org>
2021-07-16 15:29:34 -0700

    shill: Move Disable cleanup to Cellular::StopModemCallback (Take 2)
    Currently the connection is destroyed immediately in
    StopModem which can cause problems, see bug for details.
    
    BUG=b:190541087
    TEST=tast --verbose run localhost:2201 network.ShillCellularEnableAndConnect
    
    Change-Id: I53e1478230e77c6d81fd2f86e7252f0b569d8d5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3040155
    Tested-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    (cherry picked from commit 28ff4fdd2209efeb011aa75b4f38459e67f6ea58)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3077987
    Reviewed-by: Pavan Holla <pholla@google.com>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>

--- shill/cellular/cellular.h
+++ shill/cellular/cellular.h
@@ -457,6 +457,7 @@ class Cellular : public Device,
   void StopModem(Error* error, const EnabledStateChangedCallback& callback);
   void StopModemCallback(const EnabledStateChangedCallback& callback,
                          const Error& error);
+  void DestroySockets();
 
   bool ShouldBringNetworkInterfaceDownAfterDisabled() const override;
 

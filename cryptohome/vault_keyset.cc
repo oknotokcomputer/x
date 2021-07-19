ec383afca0de8783609c470ece17bbae59ebb0ed
Hardik Goyal <hardikgoyal@google.com>
2021-07-19 12:12:14 -0700

    cryptohome: Fix conditional statement in VaultKeyset
    BUG=b:194095357
    TEST=For Test team to verify
    
    Change-Id: I15700a80b84317bf8222d6f34afb98f16b528b78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3039560
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Tested-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Commit-Queue: Hardik Goyal <hardikgoyal@chromium.org>

--- cryptohome/vault_keyset.cc
+++ cryptohome/vault_keyset.cc
@@ -468,7 +468,7 @@ bool VaultKeyset::Save(const FilePath& filename) {
 }
 
 std::string VaultKeyset::GetLabel() const {
-  if (key_data_.has_value() & !key_data_->label().empty()) {
+  if (key_data_.has_value() && !key_data_->label().empty()) {
     return key_data_->label();
   }
   // Fallback for legacy keys, for which the label has to be inferred from the

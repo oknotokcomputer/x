da17a65d47ccf3fbeb1ab938c2a462fbb36c27a4
Madhav <madhavadas@google.com>
2022-07-28 06:57:54 +0000

    shill: Use home provider mccmnc
    Use home provider mccmnc instead of mobile_operator_info_->mccmnc
    to determine disabling dual-stack on L850
    
    BUG=b:235846123
    TEST=emerge-brya shill && cros deploy && check IP info
    
    Change-Id: I96735fc503762098d0a5bdc74f21fc6d566aaec1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3790367
    Tested-by: Madhav <madhavadas@google.com>
    Reviewed-by: Nagi Marupaka <nmarupaka@google.com>
    Commit-Queue: Madhav <madhavadas@google.com>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    (cherry picked from commit b74bc2076e42b37927b8d61a06af148ebe73e53f)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/3803808

--- shill/cellular/cellular_capability_3gpp.cc
+++ shill/cellular/cellular_capability_3gpp.cc
@@ -780,7 +780,7 @@ bool CellularCapability3gpp::IsDualStackSupported() {
       if (affected_device.operator_code.size() == 0 ||
           std::find(affected_device.operator_code.begin(),
                     affected_device.operator_code.end(),
-                    mobile_operator_info_->mccmnc()) !=
+                    cellular()->home_provider_info()->mccmnc()) !=
               affected_device.operator_code.end())
         return false;
     }

fc7de2a79b8232f4a6dfb17d9fc07caf715358a5
George Engelbrecht <engeg@google.com>
2023-06-09 18:13:52 -0600

    init: disable rootdev test failing on raid hosts
    If the test is within an md root this appears to fail. The state and type of the drive hosting the chroot shouldn't have an effect on unit tests ran inside of cros packages. While we investigate disable the test.
    
    BUG=b:286154453
    TEST=CQ, led
    
    Change-Id: Ibc59ed6458cf5287404bc08b75fc2e40ac7fe4c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4604816
    Reviewed-by: Alexis Savery <asavery@chromium.org>
    Commit-Queue: George Engelbrecht <engeg@google.com>
    Reviewed-by: Brian Norris <briannorris@chromium.org>
    Tested-by: George Engelbrecht <engeg@google.com>
    (cherry picked from commit eb3fdf9321f24fcda5a31de09ae1f2769edc6883)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/4714150
    Tested-by: Tomasz Jakubowski <jakubowskit@google.com>
    Owners-Override: Jack Neus <jackneus@google.com>
    Reviewed-by: Jack Neus <jackneus@google.com>
    Commit-Queue: Tomasz Jakubowski <jakubowskit@google.com>

--- init/utils_test.cc
+++ init/utils_test.cc
@@ -11,7 +11,32 @@
 #include <rootdev/rootdev.h>
 #include "init/utils.h"
 
+<<<<<<< HEAD   (55d663 shill: vpn: Set gateway address to any for wireguard)
 TEST(GetRootDevice, NoStripPartition) {
+=======
+namespace {
+
+// Commands for disk formatting utility sfdisk.
+// Specify that partition table should use gpt format.
+constexpr char kSfdiskPartitionTableTypeCommand[] = "label: gpt\n";
+// Templates for partition command (size specified in number of sectors).
+constexpr char kSfdiskCommandFormat[] = "size=1, type=%s, name=\"%s\"\n";
+constexpr char kSfdiskCommandWithAttrsFormat[] =
+    "size=1, type=%s, name=\"%s\", attrs=\"%s\"\n";
+
+// UUIDs for various partition types in gpt partition tables.
+constexpr char kKernelPartition[] = "FE3A2A5D-4F32-41A7-B725-ACCC3285A309";
+constexpr char kRootPartition[] = "3CB8E202-3B7E-47DD-8A3C-7FF2A13CFCEC";
+constexpr char kDataPartition[] = "0FC63DAF-8483-4772-8E79-3D69D8477DE4";
+constexpr char kReservedPartition[] = "2E0A753D-9E48-43B0-8337-B15192CB1B5E";
+constexpr char kRWFWPartition[] = "CAB6E88E-ABF3-4102-A07A-D4BB9BE3C1D3";
+constexpr char kEFIPartition[] = "C12A7328-F81F-11D2-BA4B-00A0C93EC93B";
+
+}  // namespace
+
+// TODO(b/286154453): Appears to fail when host OS has md array.
+TEST(GetRootDevice, DISABLED_NoStripPartition) {
+>>>>>>> CHANGE (eb3fdf init: disable rootdev test failing on raid hosts)
   base::FilePath root_dev;
   char dev_path[PATH_MAX];
   int ret = rootdev(dev_path, sizeof(dev_path), true, false);
